a60d081d03e657b55064c2acc41755bde0e9985f
bishnukandel12 <kandelketo12@gmail.com>
2025-05-16 11:24:15 +0545

    add: files

---
+++ first.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   // cout<<"we want to "<<endl<<" be good "<<"c++ progrsmmers";
+   cout<<"we want to "<<endl;
+   cout<<"be good";
+   cout<<"c++ programmers";
+   return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=2,b=5;
+
+    cout<<"value of a="<<a<<endl;
+    cout<<"value of b="<<b;
+    return 0;
+}
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a number";
+    cin>>a;                     //scanf gareko
+    cout<<"a="<<a;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numberws";
+    cin>>a;
+    cin>>b;               //cin>>a>>b;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numberws";
+    cin>>a;
+    cin>>b;               //cin>>a>>b;
+    cout<<a<<"is my luckey number and i dont like "<<b<<"number";
+   // cout<<"a="<<a<<endl;
+  //  cout<<"b="<<b<<endl;
+    return 0;
+}
---
+++ sumoftwonum.cpp
@@ -0,0 +1,15 @@
+//write a program in c++ that ask the user to input two numbers and print their sum
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter any two numbers";
+    cin>>a;
+    cin>>b;
+    int sum=a+b;
+    cout<<"sum="<<sum;
+    return 0;
+
+}                                                                                                                                                                                                                                                   `````````````                                   `                                                                                                                                                   
